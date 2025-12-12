6b3932b27e4f13f69ea7b551d5ed9db354faa5cb
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-11 23:25:39 -0800

    add WildCardNeedDemo

---
+++ src/homework/WildCardNeedDemo.java
@@ -0,0 +1,24 @@
+package homework;
+
+public class WildCardNeedDemo {
+    public static void main(String[] args) {
+        GenericStack<Integer> intStack = new GenericStack();
+        intStack.push(1);
+        intStack.push(2);
+        intStack.push(-2);
+
+        System.out.println("The max number is " + max(intStack));
+    }
+
+    public static double max(GenericStack<Integer> stack) {
+        double max = stack.pop().doubleValue();
+
+        while (!stack.isEmpty()) {
+            double value = stack.pop().doubleValue();
+            if (value > max)
+                max = value;
+        }
+
+        return max;
+    }
+}
